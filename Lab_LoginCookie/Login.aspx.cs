394b0a753055e613bc5f4f630625232efe5a5fb2
AppDev1226 <app.dev.1226@gmail.com>
2017-06-19 00:35:37 +0800

    init.

---
+++ Lab_LoginCookie/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (txtUserName.Text == "")
+        {
+            lblPrompt.Text = "Please keyin your name.";
+            return;
+        }
+
+
+        //// DB...
+
+        Response.Cookies["userName"].Value = txtUserName.Text;
+
+        HttpCookie c = Request.Cookies["backTo"];
+        if (c == null)
+            Response.Redirect("Default.aspx");
+        else
+            Response.Redirect(c.Value);
+
+
+    }
+}
